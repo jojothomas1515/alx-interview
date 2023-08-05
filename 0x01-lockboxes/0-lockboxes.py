b013b705a815b1d4f73f791186932c57894ea641
Jojo <jojothomas1515@gmail.com>
2023-08-05 07:43:20 +0100

    lock boxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,8 +4,12 @@
 
 def canUnlockAll(boxes):
     """Check if all in a lock box can be unlocked."""
+    if not isinstance(boxes, list):
+        return False
     length = len(boxes)
     unlock_box = set([0])
+    if length == 1:
+        return True
     for idx_b, box in enumerate(boxes):
         for idx in box:
             if idx_b == idx:
