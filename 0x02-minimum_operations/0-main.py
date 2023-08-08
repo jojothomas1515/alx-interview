4253f5bfcd15d93b2f4763aa0e2f80b1b84d643e
Jojo <jojothomas1515@gmail.com>
2023-08-09 00:37:31 +0100

    minops

--- 0x02-minimum_operations/0-main.py
+++ 0x02-minimum_operations/0-main.py
@@ -4,9 +4,15 @@ Main file for testing
 """
 
 minOperations = __import__('0-minoperations').minOperations
+check_prime = __import__('0-minoperations').check_prime
 
 n = 4
 print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
 
 n = 12
 print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 9
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+print(check_prime(5))
