40d6e7be8867fef7fd83b4f45d6f351548ea930b
Jojo <jojothomas1515@gmail.com>
2023-08-09 00:39:08 +0100

    minops

--- 0x02-minimum_operations/0-main.py
+++ 0x02-minimum_operations/0-main.py
@@ -12,7 +12,7 @@ print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
 n = 12
 print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
 
-n = 9
+n = 1
 print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
 
 print(check_prime(5))
